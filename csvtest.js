d28cef91a5a059ec9ae62865f0de3ba0a2b7f28b
jing.zhang <kingzad.zhang@gmail.com>
2017-01-06 11:46:55 -0500

    add new line at csvtest.js

--- csvtest.js
+++ csvtest.js
@@ -7,3 +7,5 @@ var csvCompare2 = require('./csv_service/csvCompare2');
 //csvCompare.csvDifference2('doc.b.csv', 'doc.a.csv', '","', "ClassID", "diffs.json");
 //csvCompare2.csvDifference('Classes.20161210.csv', 'Classes.20161207.csv', '","', "ClassID", "diffs.json");
 csvCompare2.csvDifference('roster.b.csv', 'roster.a.csv', '","', "hashCode", "diffs.json");
+
+//add new line
